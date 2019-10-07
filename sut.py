af0e628c5e4355d498b1536568e0ef25ba7f7b13
priscilamoreno <prismoreno12@gmail.com>
2019-10-07 20:03:10 -0300

    sut.py

--- sut.py
+++ sut.py
@@ -1,14 +1,31 @@
 import math
+from collections import defaultdict
+
+
+def productoria(lista, hasta):
+    return prod(lista[:hasta])
+
+def sumatoria(lista, hasta):
+    return sum(lista[:hasta])
 
 def area(ancho, alto):
     return ancho * alto
 
 def saludar(nombre):
     return "Hola " + nombre
-    
+
 def sumar(a, b):
     return a + b
-    
+
+def multiplicar(a, b):
+    return a * b
+
+def valorabsoluto(n):
+    n = -n
+    return n
+
+
+
 def comparar(a, b):
     if a < b:
         return "A menor que B"
@@ -16,12 +33,7 @@ def comparar(a, b):
         return "A mayor que B"
     if a == b:
         return "A y B son iguales"
-   
-def valorabsoluto(n):
-    if n < 0:
-        n = -n
-    return n
-    
+
 def costototal(producto1, producto2):
     total = sumar(producto1, producto2)
     return "El costo total es $" + str(total)
@@ -33,3 +45,32 @@ def supercalc(num):
     sqrt = math.sqrt(sum)
     return sqrt
 
+
+class CalcuClass:
+
+    def _registrar_operacion(func):
+        def operacion_decorada(self, **kwargs):
+            try:
+                getattr(self.historico, func.__name__)
+            except AttributeError:
+                self.historico[func.__name__] = []
+
+            self.historico[func.__name__].append(tuple(kwargs.values()))
+            return func(self, **kwargs)
+
+        return operacion_decorada
+
+    def __init__(self):
+        self.historico = defaultdict(list)
+
+    @_registrar_operacion
+    def suma(self, a, b):
+        return sumar(a, b)
+
+    @_registrar_operacion
+    def producto(self, a, b):
+        return multiplicar(a, b)
+
+    @_registrar_operacion
+    def sumar_varios(self, lista):
+        return sumatoria(lista, len(lista))
